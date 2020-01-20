09f99333d6f1fe38045ce942a649414b2fbf297c
Sai Vennam <svennam92@gmail.com>
2020-01-19 22:52:52 -0600

    dont validate url

--- site/public/src/settings.js
+++ site/public/src/settings.js
@@ -66,13 +66,14 @@ function getMode() {
 getMode();
 
 function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-  return !!pattern.test(str);
+  // var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+  //   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+  //   '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+  //   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+  //   '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+  //   '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  // return !!pattern.test(str);
+  return true
 }
 
 function highlightJava() {
